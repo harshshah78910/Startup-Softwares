52b13babfbe1d9c8116e343f8bf73213a91c5257
harshshah78910 <58108585+harshshah78910@users.noreply.github.com>
2019-12-01 21:09:23 +0530

    Sample on How to Add Programs to Bat file
    Add the programs to bat file and they will be used on single click for start up

---
+++ startup.bat
@@ -0,0 +1,8 @@
+start /d "C:\Program Files (x86)\XML Notepad 2007\" XmlNotepad
+
+start /d "C:\Program Files (x86)\Google\Chrome\Application\" chrome
+
+"C:\Users\harsshah\Downloads\Softwares\MobaXterm_Personal_10.9"
+
+
+@pause
